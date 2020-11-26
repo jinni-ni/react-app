3d3dc0b95ddf0a44744fa8f40fb24ffbd5aa74ee
김수진 <sjkim910115@gmail.com>
2020-11-26 21:00:24 +0900

    practice status

--- src/App.js
+++ src/App.js
@@ -1,57 +1,31 @@
 import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import "./App.css";
 
-class Subject extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>{this.props.title}</h1>
-          {this.props.sub}
-        </header>
-      </div>
-    );
-  }
-}
-
-class Toc extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is HyperText" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactvie" },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!" />
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        />
         <Subject title="React" sub="For UI!" />
-        <Toc />
+        <TOC data={this.state.contents} />
         <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
     );
